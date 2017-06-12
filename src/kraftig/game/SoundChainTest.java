9da7d58424184b38cd3dd448cca97d3f6b5bfbe6
Sam Johnson <x.smashmaster.x@gmail.com>
2017-06-12 14:46:58 -0600

    Working low pass filter

--- src/kraftig/game/SoundChainTest.java
+++ src/kraftig/game/SoundChainTest.java
@@ -12,6 +12,8 @@ public class SoundChainTest
     private final TargetDataLine inputLine;
     private final byte[] buffer;
     private final float[] leftBuffer, rightBuffer;
+    private final LowPassFilter leftLPF, rightLPF;
+    private final float[] leftOutBuffer, rightOutBuffer;
     private final SourceDataLine outputLine;
     
     SoundChainTest() throws Exception
@@ -25,6 +27,11 @@ public class SoundChainTest
         buffer = new byte[inputLine.getBufferSize()];
         leftBuffer = new float[buffer.length/4];
         rightBuffer = new float[buffer.length/4];
+        
+        leftLPF = new LowPassFilter(SAMPLE_RATE, 2000f, 1.0f);
+        rightLPF = new LowPassFilter(SAMPLE_RATE, 2000f, 1.0f);
+        leftOutBuffer = new float[buffer.length/4];
+        rightOutBuffer = new float[buffer.length/4];
         
         outputLine = AudioSystem.getSourceDataLine(format);
         outputLine.open();
@@ -47,11 +54,19 @@ public class SoundChainTest
             rightBuffer[k++] = (right + 0.5f)/32767.5f;
         }
         
+        //Apply low-pass filter.
+        int fAvailable = available/4;
+        leftLPF.apply(leftBuffer, leftOutBuffer, fAvailable);
+        rightLPF.apply(rightBuffer, rightOutBuffer, fAvailable);
+        
         //Convert back to byte data.
-        for (int i=0, k=0; k<leftBuffer.length;)
+        for (int i=0, k=0; k<fAvailable;)
         {
-            short left = (short)Math.round(leftBuffer[k]*32767.5f - 0.5f);
-            short right = (short)Math.round(leftBuffer[k++]*32767.5f - 0.5f);
+            float fLeft = Math.min(Math.max(leftOutBuffer[k], -1.0f), 1.0f);
+            float fRight = Math.min(Math.max(rightOutBuffer[k++], -1.0f), 1.0f);
+            
+            short left = (short)Math.round(fLeft*32767.5f - 0.5f);
+            short right = (short)Math.round(fRight*32767.5f - 0.5f);
             
             buffer[i++] = (byte)(left & 0xFF);
             buffer[i++] = (byte)((left >>> 8) & 0xFF);
