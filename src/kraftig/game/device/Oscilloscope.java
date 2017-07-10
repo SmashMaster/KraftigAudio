ddee2107a8f2f19b6393d3fd275bbcd4a3b6911b
Sam Johnson <x.smashmaster.x@gmail.com>
2017-07-10 15:52:29 -0600

    Row layout implemented

--- src/kraftig/game/device/Oscilloscope.java
+++ src/kraftig/game/device/Oscilloscope.java
@@ -3,14 +3,10 @@ package kraftig.game.device;
 import com.samrj.devil.math.Vec2;
 import com.samrj.devil.ui.Alignment;
 import java.util.stream.Stream;
-import kraftig.game.Main;
 import kraftig.game.Panel;
-import kraftig.game.gui.ColumnLayout;
 import kraftig.game.gui.InputJack;
 import kraftig.game.gui.Knob;
-import kraftig.game.gui.Label;
 import kraftig.game.gui.OscilloscopeScreen;
-import kraftig.game.gui.ToggleButton;
 
 public class Oscilloscope extends Panel implements Device
 {
@@ -30,15 +26,6 @@ public class Oscilloscope extends Panel implements Device
         frontInterface.add(new Knob(8.0f, new Vec2(64.0f, 0.0f), Alignment.C)
                 .setValue(0.5f)
                 .onValueChanged(v -> screen.setBrightness(MIN_BRIGHTNESS*(float)Math.pow(MAX_BRIGHTNESS/MIN_BRIGHTNESS, v))));
-        
-//        frontInterface.add(new ToggleButton(8.0f, new Vec2(-64.0f, 0.0f), Alignment.C));
-        
-        frontInterface.add(new ColumnLayout(2.0f, Alignment.C,
-                    new Label(Main.instance().getFont(), "X"),
-                    new ToggleButton(8.0f),
-                    new ToggleButton(8.0f),
-                    new ToggleButton(8.0f))
-                .setPos(new Vec2(-64.0f, 0.0f), Alignment.W));
     }
     
     @Override
